d666333518871acfc8e5d4805addf224ed96a202
Thorjan Knudsvik <t@knudsvik.com>
2020-05-22 14:39:47 +0200

    class

--- OpenServer.py
+++ OpenServer.py
@@ -1,31 +1,80 @@
-# Python script to execute Petroleum Experts OpenServer commands.
-# Written by Thorjan Knudsvik, January 2018
-
 import win32com.client
-Server = win32com.client.Dispatch("PX32.OpenServer.1")
 
-def DoSet(Sv, Val):
-    Err = Server.SetValue(Sv, Val)
-    AppName = GetAppName(Sv)
-    Err = Server.GetLastError(AppName)
-    if Err > 0:
-        print(Server.GetErrorDescription(Err))
+
+class OpenServer:
+    def __init__(self):
+        self.status = "Disconnected"
+        self.server = None
+
+    def connect(self):
+        self.server = win32com.client.Dispatch("PX32.OpenServer.1")
+        self.status = "Connected"
+        return print("OpenServer is connected")
+
+    def disconnect(self):
+        self.server = None
+        self.status = "Disconnected"
+        return print("OpenServer has been disconnected")
+
+    def DoCmd(self, Cmd):
+        if not self.status == 'Connected':
+            self.connect()
+        try:
+            Err = self.server.DoCommand(Cmd)
+            if Err > 0:
+                print(self.server.GetErrorDescription(Err))
+        except:
+            self.disconnect()
+
+    def DoSet(self, Sv, Val):
+        if not self.status == 'Connected':
+            self.connect()
+        try:
+            Err = self.server.SetValue(Sv, Val)
+            AppName = self.GetAppName(Sv)
+            Err = self.server.GetLastError(AppName)
+            if Err > 0:
+                print(self.server.GetErrorDescription(Err))
+        except:
+            self.disconnect()
+
+    def DoGet(self, Gv):
+        if not self.status == 'Connected':
+            self.connect()
+        try:
+            DoGet = self.server.GetValue(Gv)
+            AppName = self.GetAppName(Gv)
+            Err = self.server.GetLastError(AppName)
+            if Err > 0:
+                print(self.server.GetLastErrorMessage(AppName))
+            return DoGet
+        except:
+            self.disconnect()
+
+    def GetAppName(self, Strval):
+        AppName = Strval.split('.')[0]
+        if AppName not in ['PROSPER', 'MBAL', 'GAP', 'PVT', 'RESOLVE']:
+            print('Unrecognised application name in tag string')
+        return AppName
+
 
 def DoCmd(Cmd):
-    Err = Server.DoCommand(Cmd)
-    if Err > 0:
-        print(Server.GetErrorDescription(Err))
+    global _petex
+    if not '_petex' in globals():
+        _petex = OpenServer()
+    _petex.DoCmd(Cmd)
+
+
+def DoSet(Sv, Val):
+    global _petex
+    if not '_petex' in globals():
+        _petex = OpenServer()
+    _petex.DoSet(Sv, Val)
+
 
 def DoGet(Gv):
-    DoGet = Server.GetValue(Gv)
-    AppName = GetAppName(Gv)
-    Err = Server.GetLastError(AppName)
-    if Err > 0:
-        print(Server.GetLastErrorMessage(AppName))
-    return float(DoGet)
+    global _petex
+    if not '_petex' in globals():
+        _petex = OpenServer()
+    _petex.DoGet(Gv)
 
-def GetAppName(Strval):
-    AppName = Strval.split('.')[0]
-    if AppName not in ['PROSPER', 'MBAL', 'GAP', 'PVT', 'RESOLVE']:
-        print('Unrecognised application name in tag string')
-    return AppName
