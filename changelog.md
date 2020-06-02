adcf1b1751db14cf07ba7f702282bbba1cb4b15b
Thorjan Knudsvik <t@knudsvik.com>
2020-06-02 23:15:21 +0200

    Improves docstrings (resolves #8)

--- changelog.md
+++ changelog.md
@@ -1,3 +1,6 @@
+## 1.0.1
+* Improved docstrings
+
 # 1.0
 * Initial release
 
--- openserver/openserver.py
+++ openserver/openserver.py
@@ -24,11 +24,11 @@ class OpenServer:
 
     def DoCmd(self, Cmd):
         """
-        The DoCmd function is used to perform calculations and other functions such as file
-        opening and saving in the PETEX programs.
+        The DoCmd function is used to perform calculations and other functions such as file opening in an IPM tool.
+        OpenServer command strings can be found in the OpenServer User Manual or in-menu of some IPM tools.
 
         Arguments:
-            Cmd {string} -- OpenServer access string
+            Cmd {string} -- OpenServer command string
         """
         if not self.status == 'Connected':
             self.connect()
@@ -41,7 +41,9 @@ class OpenServer:
 
     def DoSet(self, Sv, Val):
         """
-        The DoSet command is used to set the value of a data item
+        The DoSet command is used to set the value of a data item.
+        OpenServer access strings can be found directly from an IPM tool by Ctrl + Right-Click mouse on a field in an
+        IPM tool, in the OpenServer User Manual or in-menu of some IPM tools.
 
         Arguments:
             Sv {string} -- OpenServer access string
@@ -60,7 +62,9 @@ class OpenServer:
 
     def DoGet(self, Gv):
         """
-        The DoGet function is used to get the value of a data item or result
+        The DoGet function is used to get the value of a data item or result.
+        OpenServer access strings can be found directly from an IPM tool by Ctrl + Right-Click mouse on a field in an
+        IPM tool, in the OpenServer User Manual or in-menu of some IPM tools.
 
         Arguments:
             Gv {string} -- OpenServer access string
@@ -93,21 +97,27 @@ class OpenServer:
             print('Unrecognised application name in tag string')
         return AppName
 
+def is_documented_by(original):
+    def wrapper(target):
+        target.__doc__ = original.__doc__
+        return target
+    return wrapper
 
+@is_documented_by(OpenServer.DoCmd)
 def DoCmd(Cmd):
     global _petex
     if not '_petex' in globals():
         _petex = OpenServer()
     _petex.DoCmd(Cmd)
 
-
+@is_documented_by(OpenServer.DoSet)
 def DoSet(Sv, Val):
     global _petex
     if not '_petex' in globals():
         _petex = OpenServer()
     _petex.DoSet(Sv, Val)
 
-
+@is_documented_by(OpenServer.DoGet)
 def DoGet(Gv):
     global _petex
     if not '_petex' in globals():
