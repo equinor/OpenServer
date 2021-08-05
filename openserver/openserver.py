97ea03f46e7e8d937a2fe37000b698bed1ee6b0a
Marc Trevoux <69000095+MTrevoux@users.noreply.github.com>
2021-08-05 15:55:28 +0100

    Update openserver.py
    adding context manager

--- openserver/openserver.py
+++ openserver/openserver.py
@@ -126,6 +126,27 @@ class OpenServer:
 
     def GetAppName(self, Strval):
         return Strval.split('.')[0]
+
+class OSconnect():
+    """
+    Custom context for managing connections with the server and preventing licence blockage.
+    In case of any error, the script will automatically disconnect from the server and then raising an exception.
+    This class shall be used with a "With" statement. 
+    
+    Example:
+    With OSconnect() as c:
+        c.DoSet('target', 'value')
+        ...do other things...
+    """
+    def __init__(self):
+        self.server = OpenServer()
+
+    def __enter__(self):
+        self.server.connect()
+        return self.server
+
+    def __exit__(self, *args):
+        self.server.disconnect()
     
 def is_documented_by(original):
     def wrapper(target):
